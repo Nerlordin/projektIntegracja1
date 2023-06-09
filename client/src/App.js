8d1aef0edf1d187acc1f589e4c731f03f4133f4e
Nerlordin <faironmail@gmail.com>
2023-06-09 11:29:00 +0200

    Filter the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import './App.css'; // Importowanie pliku CSS
-import DaneComponent from './DaneComponent';
+import './App.css';
 
 const App = () => {
   const [salaryData, setSalaryData] = useState([]);
   const [dane, setDane] = useState([]);
+  const [searchYear, setSearchYear] = useState('');
 
   useEffect(() => {
     fetchData();
@@ -23,6 +23,14 @@ const App = () => {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearchYear(e.target.value);
+  };
+
+  const filteredData = salaryData.filter((item) =>
+  item.rok.toString().toLowerCase().includes(searchYear.toLowerCase())
+);
+
   const removeDuplicates = (data, key) => {
     return data.filter((item, index, self) => {
       return index === self.findIndex((i) => i[key] === item[key]);
@@ -34,30 +42,40 @@ const App = () => {
       <h1>MERN Starter</h1>
       <h2>Salary Data:</h2>
 
+      <input
+        type="text"
+        value={searchYear}
+        onChange={handleSearchChange}
+        placeholder="Wyszukaj rok..."
+      />
+
       <table>
         <thead>
           <tr>
             <th>Kraj</th>
-            {salaryData.map((item) => (
+            {filteredData.map((item) => (
               <th key={item.rok}>{item.kraj}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {salaryData.map((item) => (
-            <tr key={item.kraj}>
+          {filteredData.map((item) => (
+            <tr key={item.rok}>
               <td>{item.rok}</td>
-              {salaryData.map((d) => (
-                <td key={d.rok}>
-                  {d.srednia_wyplata}
-                  {dane.map((daneItem) => {
-                    const krajData = daneItem.kraje.find(
-                      (kraj) => kraj.kraj === d.kraj
-                    );
-                    return krajData && krajData.cena_paliwa;
-                  })}
-                </td>
-              ))}
+              {filteredData.map((d) => {
+                const krajData = dane.find((daneItem) =>
+                  daneItem.kraje.some((kraj) => kraj.kraj === d.kraj)
+                );
+                const cena_paliwa = krajData
+                  ? krajData.kraje.find((kraj) => kraj.kraj === d.kraj).cena_paliwa
+                  : null;
+                return (
+                  <td key={d.rok}>
+                    {d.srednia_wyplata}
+                    {cena_paliwa && <span>, Cena paliwa: {cena_paliwa}</span>}
+                  </td>
+                );
+              })}
             </tr>
           ))}
         </tbody>

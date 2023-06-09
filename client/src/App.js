df5035fc2ed93046618beacf6fc05dd18b4d9008
Nerlordin <faironmail@gmail.com>
2023-06-09 16:14:18 +0200

    Add User and edit css style

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 const App = () => {
   const [salaryData, setSalaryData] = useState([]);
   const [dane, setDane] = useState([]);
-  const [searchYear, setSearchYear] = useState('');
+  const [loaded, setLoaded] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(null);
 
   useEffect(() => {
     fetchData();
@@ -14,90 +15,62 @@ const App = () => {
 
   const fetchData = async () => {
     try {
+      const array = [];
       const response = await axios.get('/api/salary');
       const response2 = await axios.get('http://localhost:5000/dane');
       setDane(response2.data);
-      setSalaryData(response.data.dane);
+      for (let i = 0; i < 3; i++) {
+        const grouped = response.data.dane.filter((kraj) => kraj.rok === 2020 + i);
+        array.push(grouped);
+      }
+      setSalaryData(array);
+      setLoaded(true);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleSearchChange = (e) => {
-    setSearchYear(e.target.value);
+  const handleYearChange = (event) => {
+    const year = parseInt(event.target.value);
+    setSelectedYear(year);
   };
 
-  const filteredData = salaryData.filter((item) =>
-  item.rok.toString().toLowerCase().includes(searchYear.toLowerCase())
-);
-
-  const removeDuplicates = (data, key) => {
-    return data.filter((item, index, self) => {
-      return index === self.findIndex((i) => i[key] === item[key]);
-    });
-  };
+  const filteredSalaryData = selectedYear ? salaryData.filter((item) => item[0].rok === selectedYear) : salaryData;
 
   return (
     <div className="container">
       <h1>MERN Starter</h1>
       <h2>Salary Data:</h2>
 
-      <input
-        type="text"
-        value={searchYear}
-        onChange={handleSearchChange}
-        placeholder="Wyszukaj rok..."
-      />
+      <label htmlFor="year-select">Select Year:</label>
+      <select id="year-select" value={selectedYear || ''} onChange={handleYearChange}>
+        <option value="">All</option>
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+      </select>
 
       <table>
         <thead>
           <tr>
             <th>Kraj</th>
-            {filteredData.map((item) => (
-              <th key={item.rok}>{item.kraj}</th>
-            ))}
+            {loaded &&
+              filteredSalaryData[0].map((item) => <th key={item.rok}>{item.kraj}</th>)}
           </tr>
         </thead>
         <tbody>
-          {filteredData.map((item) => (
-            <tr key={item.rok}>
-              <td>{item.rok}</td>
-              {filteredData.map((d) => {
-                const krajData = dane.find((daneItem) =>
-                  daneItem.kraje.some((kraj) => kraj.kraj === d.kraj)
-                );
-                const cena_paliwa = krajData
-                  ? krajData.kraje.find((kraj) => kraj.kraj === d.kraj).cena_paliwa
-                  : null;
-                return (
-                  <td key={d.rok}>
-                    {d.srednia_wyplata}
-                    {cena_paliwa && <span>, Cena paliwa: {cena_paliwa}</span>}
-                  </td>
-                );
-              })}
+          {filteredSalaryData.map((item, index) => (
+            <tr key={index}>
+              <td>{item[0].rok}</td>
+              {item.map((kraj) => (
+                <td key={kraj.kraj}>{kraj.srednia_wyplata}</td>
+              ))}
             </tr>
           ))}
         </tbody>
       </table>
 
       <h2>Wykres zmian:</h2>
-      <LineChart width={800} height={400} data={salaryData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="rok" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {salaryData.map((item) => (
-          <Line
-            key={item.kraj}
-            type="monotone"
-            dataKey={item.kraj}
-            name={item.kraj}
-            stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
-          />
-        ))}
-      </LineChart>
     </div>
   );
 };

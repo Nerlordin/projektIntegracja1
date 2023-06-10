8101eaea0544c943b8d0ed123c01b8e45b4b32e0
Nerlordin <faironmail@gmail.com>
2023-06-10 23:57:05 +0200

    Fix minor error

--- client/src/components/Wykres.js
+++ client/src/components/Wykres.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const Wykres = ({ data, yearsToShow, loading }) => {
+const Wykres = ({ data, yearsToShow, }) => {
   const filteredData = data.filter((item) => yearsToShow.includes(item.rok));
 
   return (
@@ -11,7 +11,7 @@ const Wykres = ({ data, yearsToShow, loading }) => {
       <CartesianGrid strokeDasharray="3 3" />
       <Tooltip />
       <Legend />
-      {loading && filteredData[0].map((kraj) => (
+      {loading && filteredData[0].kraje.map((kraj) => (
         <Line key={kraj.kraj} type="monotone" dataKey={`kraje.${kraj.kraj}.cena_paliwa`} name={kraj.kraj} />
       ))}
     </LineChart>
